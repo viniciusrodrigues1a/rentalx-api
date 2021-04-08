2d56aec60b772911248c4c0ff59ad784a11ec357
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-08 01:38:49 -0300

    chapter II

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,56 @@
+import csvParser from "csv-parse";
+import fs from "fs";
+
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
+      const stream = fs.createReadStream(file.path);
+
+      const parsedFile = csvParser();
+
+      stream.pipe(parsedFile);
+
+      parsedFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on("error", (err) => reject(err));
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.forEach((category) => {
+      const { name, description } = category;
+
+      const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+      if (!categoryAlreadyExists) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.use("/categories", categoriesRoutes);
 app.use("/specifications", specificationsRoutes);
