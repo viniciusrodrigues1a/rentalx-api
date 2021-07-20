8540368818533313b04f92ac994950c40fab1ab9
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-07-20 15:13:28 -0300

    adding new use case to update user's avatar

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -35,6 +35,10 @@ export async function ensureAuthenticated(
       throw new AppError("User doesn't exist", 401);
     }
 
+    request.user = {
+      id: user.id,
+    };
+
     next();
   } catch (err) {
     throw new AppError("Invalid token", 401);
