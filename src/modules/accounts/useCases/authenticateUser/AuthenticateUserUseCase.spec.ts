d5114e0f793d3664091d3ae92044fe47400a83bc
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-07-21 17:40:24 -0300

    chapter 4 - adding tests and configuring absolute imports

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,60 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let sut: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    sut = new AuthenticateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to authenticate a user", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000123",
+      email: "user@email.com",
+      name: "user",
+      password: "1234",
+    };
+    await createUserUseCase.execute(user);
+
+    const result = await sut.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should NOT be able to authenticate with a non-existent user", async () => {
+    await expect(
+      sut.execute({
+        email: "invalid@email.com",
+        password: "1234",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should NOT be able to authenticate with incorrect password", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000123",
+      email: "user@email.com",
+      name: "user",
+      password: "1234",
+    };
+    await createUserUseCase.execute(user);
+
+    await expect(
+      sut.execute({
+        email: user.email,
+        password: "incorrect_pa55",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let sut: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    sut = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+  });
+
+  it("should be able to create a new Category", async () => {
+    const category = {
+      name: "My category",
+      description: "My category description",
+    };
+
+    await sut.execute(category);
+
+    const createdCategory = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(createdCategory).toHaveProperty("id");
+  });
+
+  it("should NOT be able to create a new Category if name is already taken", async () => {
+    const category = {
+      name: "My category",
+      description: "My category description",
+    };
+
+    await sut.execute(category);
+
+    await expect(sut.execute(category)).rejects.toBeInstanceOf(AppError);
+  });
+});
