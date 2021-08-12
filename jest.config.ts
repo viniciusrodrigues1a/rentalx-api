01084b98c295583f7fb0869e3a73b45315ca71ee
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-08-12 16:50:02 -0300

    making jest understand root imports

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,14 @@
+import { pathsToModuleNameMapper } from "ts-jest/utils";
+
+import { compilerOptions } from "./tsconfig.json";
+
 export default {
   bail: true,
   clearMocks: true,
   coverageProvider: "v8",
   preset: "ts-jest",
   testMatch: ["**/*.spec.ts"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/",
+  }),
 };
