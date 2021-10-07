9bc71571352e39e0296c0b37d49bcd827c4ee4e2
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-10-07 16:35:27 -0300

    adding route to create a rental

--- requirements/modules/cars/requirements.md
+++ requirements/modules/cars/requirements.md
@@ -52,3 +52,4 @@ Deve ser possível cadastrar um aluguel.
 O aluguel deve ter duração mínima de 1 hora.
 Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário.
 Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
+O usuário deve estar logado na aplicação
---
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -0,0 +1,40 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
+import { Rental } from "../entities/Rental";
+
+class RentalsRepository implements IRentalsRepository {
+  private repository: Repository<Rental>;
+
+  constructor() {
+    this.repository = getRepository(Rental);
+  }
+
+  findOpenRentalByCar(car_id: string): Promise<Rental> {
+    return this.repository.findOne({ car_id });
+  }
+
+  findOpenRentalByUser(user_id: string): Promise<Rental> {
+    return this.repository.findOne({ user_id });
+  }
+
+  async create({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = this.repository.create({
+      car_id,
+      expected_return_date,
+      user_id,
+    });
+
+    await this.repository.save(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepository };
