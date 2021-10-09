105b8eb13b5215e3a919ef64fb81ca75d5ef0e62
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-10-09 02:32:40 -0300

    adding integration tests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,34 +1,3 @@
-import "reflect-metadata";
-import "@shared/container";
-
-import express, { Response } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-
-import { AppError } from "@shared/errors/AppError";
-import createConnection from "@shared/infra/typeorm";
-
-import swaggerConfig from "../../../swagger.json";
-import { router } from "./routes";
-
-const app = express();
-
-createConnection();
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
-
-app.use(router);
-
-app.use((err: Error, _, response: Response, __) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({ message: err.message });
-  }
-
-  return response.status(500).json({
-    status: "error",
-    message: `Internal server error - ${err.message}`,
-  });
-});
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running"));
